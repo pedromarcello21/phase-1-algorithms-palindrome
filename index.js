30a46e205383f6cac2adc4bca176939365e23874
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-06-14 14:31:17 -0400

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  // function takes a string and checks if it's spelled the same backwards.
+  // yo isn't a palindrome because yo !== oy
+  // racecar is a palindrome because racecar === racecar
 
+  word = word.toLowerCase();
+
+  return word === word.split("").reverse().join("")
+}
 /* 
   Add your pseudocode here
+  if word equals reverse word, true, otherwise false
 */
 
 /*
   Add written explanation of your solution here
+  utilize knowledge of .reverse() and .join("") methods on arrays 
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
